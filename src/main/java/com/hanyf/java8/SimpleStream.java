a6f78f8754bd3ffd1a979b43fe8f1457e883a35e
hanyf <hanyf493@163.com>
2018-03-17 10:53:34 +0800

    'songlili2'

--- src/main/java/com/hanyf/java8/SimpleStream.java
+++ src/main/java/com/hanyf/java8/SimpleStream.java
@@ -1,11 +1,6 @@
 package com.hanyf.java8;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
